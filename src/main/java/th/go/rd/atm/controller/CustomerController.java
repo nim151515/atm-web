69a087a4f0bd666daf3679b9bf3aa95b2d43040e
wipaporn <k_anny@outlook.co.th>
2020-08-26 14:18:07 +0700

    add customer form

--- src/main/java/th/go/rd/atm/controller/CustomerController.java
+++ src/main/java/th/go/rd/atm/controller/CustomerController.java
@@ -2,21 +2,34 @@ package th.go.rd.atm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.ArrayList;
 
 @Controller
 public class CustomerController {
+    private ArrayList<Customer> customers = new ArrayList<>();
+//    @RequestMapping("/customer")
 
-    @RequestMapping("/customer")
+    @GetMapping("/customer")
     public String getCustomerPage(Model model) {
-        ArrayList<Customer> customers = new ArrayList<>();
-        customers.add(new Customer(1,"Peter","1234"));
-        customers.add(new Customer(2,"Nancy","2345"));
-        customers.add(new Customer(3,"Rick","3456"));
+//        ArrayList<Customer> customers = new ArrayList<>();
+//        customers.add(new Customer(1,"Peter","1234"));
+//        customers.add(new Customer(2,"Nancy","2345"));
+//        customers.add(new Customer(3,"Rick","3456"));
 
         model.addAttribute("allCustomers", customers);
         return "customer";
     }
+
+    @PostMapping("/customer")
+    public String registerCustomer(@ModelAttribute Customer customer, Model model) {
+        customers.add(customer);
+        model.addAttribute("allCustomers", customers);
+        return "redirect:customer";
+    }
+
 }
 
